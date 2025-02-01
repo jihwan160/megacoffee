c91f037e9645927396246fe6aa483b08ecfa77ea
jihwan <dark160@naver.com>
2025-02-01 15:29:59 +0900

    sec2Slide

--- js/index.js
+++ js/index.js
@@ -66,7 +66,7 @@ updateGnb();
 // 스와이퍼js와 index.js가 충돌이나서 DOMContentLoaded 을 하면 충돌안남
 document.addEventListener('DOMContentLoaded', () => {
   // 스와이퍼 슬라이드이미지 여러개 만들기
-  const swiperWrapper = document.querySelector('.swiper-wrapper');
+  const swiperWrapper = document.querySelector('.sec1 .swiper-wrapper');
   // 이미지 배열
   const imgUrl = [
     '/img/main/slide1.jpg',
@@ -114,5 +114,55 @@ document.addEventListener('DOMContentLoaded', () => {
   
   
     });
+
+    const swiperWrapper2 = document.querySelector('.right .swiper-wrapper');
+
+    const imgUrl2 = [
+      {
+        img : 'img/main/sec2/slideImg1.jpg',
+        alt : '왕메가 딸기라떼',
+        title : '왕메가 딸기라떼',
+        text : 'Big Mega Strawberry Latte',
+        desc : ' 이보다 더 딸기가 많을 순 없다 왕메가사이즈에 듬뿍 넣은 딸기과육과 부드러운 우유의 핑크빛 하모니를 하루종일 느낄 수 있는 딸기시즌 한정 왕메가 딸기라떼',
+      },
+      // 'img/main/sec2/slideImg2.jpg',
+      // 'img/main/sec2/slideImg3.jpg',
+      // 'img/main/sec2/slideImg4.jpg',
+      // 'img/main/sec2/slideImg5.jpg',
+      // 'img/main/sec2/slideImg6.jpg',
+      // 'img/main/sec2/slideImg7.jpg',
+    ]
+
+    imgUrl2.forEach((url,index) => {
+      const slide = document.createElement('div');
+      slide.className = 'swiper-slide';
+
+      const img = document.createElement('img');
+      img.src = url.img;
+      img.alt = url.alt;
+
+      const text = document.createElement('div');
+      text.className = 'textArea';
+
+      const title = document.createElement('h2');
+      title.className = 'title';
+      title.textContent = url.title;
+
+      const subtitle = document.createElement('p');
+      subtitle.className = 'subTitle';
+      subtitle.textContent = url.text;
+
+      const desc = document.createElement('p');
+      desc.className = 'desc';
+      desc.textContent = url.desc;
+
+      slide.appendChild(img);
+      text.appendChild(title);
+      text.appendChild(subtitle);
+      text.appendChild(desc);
+
+      slide.appendChild(text)
+      swiperWrapper2.appendChild(slide);
+    })
 })
 
